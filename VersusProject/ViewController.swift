e54d99d61b4c8fee437fe366498272bbcd384c76
Mitchell <mitchellt1912@gmail.com>
2022-04-22 11:17:26 -0500

    Initial Commit

---
+++ VersusProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VersusProject
+//
+//  Created by Mitchell Tan on 4/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
