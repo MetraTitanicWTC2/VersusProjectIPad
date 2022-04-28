2de2b191b96c28b9d96b574f54e26901c201120e
Dylan Kwan <dkwan2216@stu.d214.org>
2022-04-28 11:00:12 -0500

    added touch gesture to indicate selected character

--- VersusProject/SecondViewController.swift
+++ VersusProject/SecondViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 class SecondViewController: UIViewController {
 
     var playerChoice = 0
+    var selectedImage = ""
     
+    @IBOutlet weak var selectionImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,7 +21,31 @@ class SecondViewController: UIViewController {
     }
     
 
-   
+    @IBAction func whenRexaTapped(_ sender: UITapGestureRecognizer) {
+        playerChoice = 0
+        selectedImage = "Rexa"
+        selectionImage.image = UIImage(named: selectedImage)
+    }
+    
+    @IBAction func whenArcturusTapped(_ sender: UITapGestureRecognizer) {
+        playerChoice = 1
+        selectedImage = "Arcturus"
+        selectionImage.image = UIImage(named: selectedImage)
+    }
+    
+    
+    @IBAction func whenDeimosTapped(_ sender: UITapGestureRecognizer) {
+        playerChoice = 2
+        selectedImage = "Deimos"
+        selectionImage.image = UIImage(named: selectedImage)
+    }
+    
+    @IBAction func whenFermiTapped(_ sender: UITapGestureRecognizer) {
+        playerChoice = 3
+        selectedImage = "Deimos"
+        selectionImage.image = UIImage(named: selectedImage)
+    }
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let fvc = segue.destination as! ThirdViewController
