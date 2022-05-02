eefd3fdc3f97ef3d233172223848620ade759e20
Mitchell <mitchellt1912@gmail.com>
2022-05-02 11:06:23 -0500

    commit

--- VersusProject/ThirdViewController.swift
+++ VersusProject/ThirdViewController.swift
@@ -21,7 +21,7 @@ class ThirdViewController: UIViewController {
     let fermi = character(name: "Fermi", health: 3000, defense: 0.35, attackLow: 300, attackHigh: 400, heal: 1.2)
    
     
-    
+
     @IBOutlet weak var playerHealthLabel: UILabel!
     @IBOutlet weak var enemyHealthLabel: UILabel!
     @IBOutlet weak var enemyImageView: UIImageView!
@@ -45,7 +45,7 @@ class ThirdViewController: UIViewController {
         
         // Do any additional setup after loading the view.
     }
-    
+
     
 
  
