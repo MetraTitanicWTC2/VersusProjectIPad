4c18fd7aff00cb42b6f86054ac5ad7caf0adc995
Amanda Reyes <areyes4368@stu.d214.org>
2022-05-16 11:11:22 -0500

    Added delays

--- VersusProject/ThirdViewController.swift
+++ VersusProject/ThirdViewController.swift
@@ -86,7 +86,7 @@ class ThirdViewController: UIViewController {
 
         enemyHealthLabel.text = "Health: \(enemyHealth)"
         playerAttackLog.text = "You attacked for \(attack) damage!"
-            
+            sleep(1)
             let enemyChoice = enemyChoiceArracy.randomElement()
             if enemyChoice == "attack" {
                 enemyHealth -= attack
@@ -116,7 +116,6 @@ class ThirdViewController: UIViewController {
     }
     @IBAction func whenDefending(_ sender: Any) {
         let attack = 0
-        
         let enemyChoice = enemyChoiceArracy.randomElement()
         if enemyChoice == "attack" {
             enemyHealth -= attack
@@ -126,15 +125,17 @@ class ThirdViewController: UIViewController {
             let enemyAttackValue = enemyAttack(enemyAttackLow: enemyAttackLow, enemyAttackHigh: enemyAttackHigh)
             let enemyAttackDouble = (1 - playerDefense) * Double(enemyAttackValue)
             let enemyAttack = Int(enemyAttackDouble)
+            sleep(1)
             playerDefendedEnemyAttackHealthUpdate(attack: enemyAttack, originalAttack: enemyAttackValue)
             enemyAttackHealthUpdate(attack: enemyAttackValue)
         } else if enemyChoice == "defend" {
             enemyDefendedPlayerAttack(attack: attack)
             playerAttackLog.text = "You defended against 0 damage!"
+            sleep(1)
         } else if enemyChoice == "heal" {
             enemyHealing(enemyHealLow: enemyHealLow, enemyHealHigh: enemyHealHigh, maxHealth: enemyMaxHealth)
             playerAttackLog.text = "You defended against 0 damage!"
-
+            sleep(1)
         }
         
         
@@ -155,6 +156,7 @@ class ThirdViewController: UIViewController {
             playerHealth = playerMaxHealth
             playerHealthLabel.text = "Health: \(playerHealth)"
             playerAttackLog.text = "You healed to full health!"
+            sleep(1)
             intHeal -= 20
             special.text = "SP: \(intHeal)"
 
@@ -172,10 +174,6 @@ class ThirdViewController: UIViewController {
             healButton.isEnabled = false
             
         }
-            
-            
-        
-        
         let enemyChoice = enemyChoiceArracy.randomElement()
         if enemyChoice == "attack" {
             let enemyAttackValue = enemyAttack(enemyAttackLow: enemyAttackLow, enemyAttackHigh: enemyAttackHigh)
@@ -256,6 +254,7 @@ class ThirdViewController: UIViewController {
     
     func gameResultDetector() {
         if playerHealth <= 0 {
+            sleep(1)
             view.isHidden = true
             let playerLoseAlert = UIAlertController(title: "You Lose!", message: nil, preferredStyle: UIAlertController.Style.alert)
             let menu = UIAlertAction(title: "Main Menu", style: .default, handler: {action in self.performSegue(withIdentifier: "Menu", sender: Any?.self)})
@@ -275,6 +274,7 @@ class ThirdViewController: UIViewController {
         }
     
         if enemyHealth <= 0 {
+            sleep(1)
             view.isHidden = true
             let playerWinAlert = UIAlertController(title: "You Win!", message: nil, preferredStyle: UIAlertController.Style.alert)
             let menu = UIAlertAction(title: "Main Menu", style: .cancel, handler: {action in self.performSegue(withIdentifier: "Menu", sender: Any?.self)})
